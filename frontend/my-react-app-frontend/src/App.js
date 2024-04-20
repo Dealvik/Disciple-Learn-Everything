9bbc6f9ff79287774d50dddca9f38daf7d949a3b
David <dealvik@gmail.com>
2024-04-19 21:57:13 -0400

    submit unit feature (title, content)

--- frontend/my-react-app-frontend/src/App.js
+++ frontend/my-react-app-frontend/src/App.js
@@ -2,40 +2,69 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, addDoc, collection } from "firebase/firestore";
 
 function App() {
   const [lessonList, setLessonList] = useState([]);
 
+  // New Unit states
+  const [newUnitName, setNewUnitName] = useState("");
+  const [newContent, setNewContent] = useState("");
+
   const lessonsCollectionRef = collection(db, "lessons");
 
-  useEffect(() => {
-    const getLessonList = async () => {
-      // Read the data
-      // Set the lesson list
-      try {
-        const data = await getDocs(lessonsCollectionRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setLessonList(filteredData);
-        console.log(filteredData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  const getLessonList = async () => {
+    // Read the data
+    // Set the lesson list
+    try {
+      const data = await getDocs(lessonsCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setLessonList(filteredData);
+      console.log(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
+  useEffect(() => {
     getLessonList();
   }, []);
+
+  const onSubmitUnit = async () => {
+    try {
+      await addDoc(lessonsCollectionRef, {
+        title: newUnitName,
+        dateCreated: "4/19/2024",
+        content: newContent,
+      });
+
+      getLessonList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="App">
       <Auth />
 
       <div>
-        <input placeholder="Lesson title..." />
-        <input placeholder="Content..." />
+        <input
+          placeholder="Unit name..."
+          onChange={(e) => {
+            setNewUnitName(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Content..."
+          onChange={(e) => {
+            setNewContent(e.target.value);
+          }}
+        />
+        <button onClick={onSubmitUnit}>Submit Unit</button>
       </div>
 
       <div>
